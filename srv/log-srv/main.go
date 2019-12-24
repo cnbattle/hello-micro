f0c8c9e656d1ac194dcfaa898a36a78a7d6563c8
cnbattle <qiaicn@gmail.com>
2019-12-24 16:34:44 +0800

    add go.micro.hello.micro.srv.log service

---
+++ srv/log-srv/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+
+	proto "github.com/cnbattle/hello-micro/proto/log"
+	"github.com/micro/go-micro"
+	"github.com/micro/go-micro/util/log"
+)
+
+type Sub struct {
+}
+
+func (s Sub) Process(ctx context.Context, evt *proto.LogEvt) error {
+	// 业务逻辑
+	log.Logf("[sub] 收到日志: %v", evt.Msg)
+	return nil
+}
+
+func main() {
+	service := micro.NewService(
+		micro.Name("go.micro.hello.micro.srv.log"),
+	)
+
+	service.Init()
+
+	_ = micro.RegisterSubscriber("go.micro.hello.micro.topic.log", service.Server(), &Sub{})
+
+	if err := service.Run(); err != nil {
+		panic(err)
+	}
+}
