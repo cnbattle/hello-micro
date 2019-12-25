8cbd62972ff12f4f135c0aa33e9150e244bcf940
cnbattle <qiaicn@gmail.com>
2019-12-25 17:48:00 +0800

    update jwt_test.go

--- kernel/jwt/jwt_test.go
+++ kernel/jwt/jwt_test.go
@@ -1,13 +1,20 @@
 package jwt
 
 import (
+	"github.com/cnbattle/hello-micro/kernel/utils"
+	"github.com/google/uuid"
 	"log"
 	"testing"
 )
 
 // TestGenerateToken
 func TestGenerateToken(t *testing.T) {
-	generateToken, err := GenerateToken("uid", "nickname", "avatar", "gender", "signature")
+	uid := uuid.New().String()
+	nickname := utils.RandString(6)
+	avatar := "https://ruan.co/" + utils.RandString(6)
+	gender := utils.RandString(1)
+	signature := utils.RandString(128)
+	generateToken, err := GenerateToken(uid, nickname, avatar, gender, signature)
 	if err != nil {
 		t.Error(err)
 	}
@@ -22,7 +29,12 @@ func TestParseToken(t *testing.T) {
 		t.Error("testing oldToken is not error")
 		return
 	}
-	generateToken, err := GenerateToken("uid", "nickname", "avatar", "gender", "signature")
+	uid := uuid.New().String()
+	nickname := utils.RandString(6)
+	avatar := "https://ruan.co/" + utils.RandString(6)
+	gender := utils.RandString(1)
+	signature := utils.RandString(128)
+	generateToken, err := GenerateToken(uid, nickname, avatar, gender, signature)
 	if err != nil {
 		t.Error(err)
 	}
@@ -34,14 +46,35 @@ func TestParseToken(t *testing.T) {
 
 // TestVerifyToken
 func TestVerifyToken(t *testing.T) {
-	tests := []struct {
+	type testsStruct struct {
 		name string
 		args string
 		want bool
-	}{
-		// TODO: Add test cases.
-		{"1", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NzcyNjM2ODYsImlhdCI6MTU3NzI2MzY3NiwidWlkIjoidWlkIiwibmlja25hbWUiOiJuaWNrbmFtZSIsImF2YXRhciI6ImF2YXRhciIsImdlbmRlciI6ImdlbmRlciIsInNpZ25hdHVyZSI6InNpZ25hdHVyZSIsInBlcm1pc3Npb25zIjpudWxsfQ.jUXsL4WMM2lJKu56DMHpHTwtPGvAixQwfxxlyI9X0bM", false},
 	}
+	var tests []testsStruct
+
+	tests = append(tests, testsStruct{
+		name: "error",
+		args: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NzcyNjM2ODYsImlhdCI6MTU3NzI2MzY3NiwidWlkIjoidWlkIiwibmlja25hbWUiOiJuaWNrbmFtZSIsImF2YXRhciI6ImF2YXRhciIsImdlbmRlciI6ImdlbmRlciIsInNpZ25hdHVyZSI6InNpZ25hdHVyZSIsInBlcm1pc3Npb25zIjpudWxsfQ.jUXsL4WMM2lJKu56DMHpHTwtPGvAixQwfxxlyI9X0bM",
+		want: false,
+	})
+	for i := 0; i < 10; i++ {
+		uid := uuid.New().String()
+		nickname := utils.RandString(6)
+		avatar := "https://ruan.co/" + utils.RandString(6)
+		gender := utils.RandString(1)
+		signature := utils.RandString(128)
+		generateToken, err := GenerateToken(uid, nickname, avatar, gender, signature)
+		if err != nil {
+			t.Errorf("GenerateToken() error: %v", err)
+		}
+		tests = append(tests, testsStruct{
+			name: "true",
+			args: generateToken,
+			want: true,
+		})
+	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := VerifyToken(tt.args); got != tt.want {
