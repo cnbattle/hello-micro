768c1f7ea44c223557f75b604e7c3937d4fbc1fd
cnbattle <qiaicn@gmail.com>
2019-12-31 20:35:41 +0800

    dev auth-srv ing

--- srv/auth-srv/main.go
+++ srv/auth-srv/main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	"github.com/cnbattle/hello-micro/pkg/config"
 	"log"
 
 	proto "github.com/cnbattle/hello-micro/proto/auth"
 	"github.com/cnbattle/hello-micro/srv/auth-srv/handler"
+	_ "github.com/joho/godotenv/autoload"
 	"github.com/micro/go-micro"
 )
 
 var (
 	// microName
-	microName = "com.cnbattle.svr.auth"
+	microName = config.GetDefaultEnv("MICRO_NAME", "com.cnbattle.svr.auth")
 )
 
 func main() {
