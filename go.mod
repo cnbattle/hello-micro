e8e6bf7d32fbc5e9e34bf790bf8686605e3f1310
cnbattle <qiaicn@gmail.com>
2020-01-01 10:59:50 +0800

    Registry use etcd

--- go.mod
+++ go.mod
@@ -3,15 +3,13 @@ module github.com/cnbattle/hello-micro
 go 1.13
 
 require (
-	github.com/bradfitz/gomemcache v0.0.0-20190913173617-a41fca850d0b // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/golang/protobuf v1.3.2
-	github.com/gomodule/redigo v2.0.0+incompatible // indirect
 	github.com/google/uuid v1.1.1
 	github.com/jinzhu/gorm v1.9.11
 	github.com/joho/godotenv v1.3.0
 	github.com/micro/cli v0.2.0
 	github.com/micro/go-micro v1.18.0
-	github.com/micro/micro v1.18.0 // indirect
+	github.com/micro/go-plugins v1.5.1
 	github.com/silenceper/wechat v2.0.1+incompatible
 )
--- srv/auth-srv/main.go
+++ srv/auth-srv/main.go
@@ -8,6 +8,8 @@ import (
 	"github.com/cnbattle/hello-micro/srv/auth-srv/handler"
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/micro/go-micro"
+	"github.com/micro/go-micro/registry"
+	"github.com/micro/go-plugins/registry/etcdv3"
 )
 
 var (
@@ -16,8 +18,16 @@ var (
 )
 
 func main() {
+	// etcdv3 registerDrive
+	registerDrive := etcdv3.NewRegistry(func(options *registry.Options) {
+		// etcd 地址
+		options.Addrs = []string{"127.0.0.1:2379"}
+		// etcd 用户名密码,如果设置的话
+		etcdv3.Auth("root", "password")(options)
+	})
 	service := micro.NewService(
 		micro.Name(microName),
+		micro.Registry(registerDrive),
 	)
 
 	service.Init()
