0b24a09275218ae09f10f8ded55dc811c662b433
cnbattle <qiaicn@gmail.com>
2020-06-08 21:27:35 +0800

    update

--- srv/auth-srv/main.go
+++ srv/auth-srv/main.go
@@ -9,13 +9,6 @@ import (
 	_ "github.com/joho/godotenv/autoload"
 
 	"github.com/micro/go-micro/v2"
-	"github.com/micro/go-micro/v2/broker"
-	"github.com/micro/go-micro/v2/registry"
-	"github.com/micro/go-micro/v2/transport"
-
-	"github.com/micro/go-plugins/broker/rabbitmq"
-	"github.com/micro/go-plugins/registry/etcdv3"
-	"github.com/micro/go-plugins/transport/nats"
 )
 
 var (
@@ -25,25 +18,25 @@ var (
 
 func main() {
 	// etcdv3 registerDrive
-	registerDrive := etcdv3.NewRegistry(func(options *registry.Options) {
-		options.Addrs = []string{"127.0.0.1:2379"}
-	})
-
-	// rabbitmq brokerDrive
-	brokerDrive := rabbitmq.NewBroker(func(options *broker.Options) {
-		options.Addrs = []string{"127.0.0.1:5672"}
-	})
-
-	// nats transportDrive
-	transportDrive := nats.NewTransport(func(options *transport.Options) {
-		options.Addrs = []string{"127.0.0.1:4222"}
-	})
+	//var options registry.Options
+	//options.Addrs =  []string{"127.0.0.1:2379"}
+	//registerDrive := etcdv3.NewRegistry(options...)
+	//
+	//// rabbitmq brokerDrive
+	//brokerDrive := rabbitmq.NewBroker(func(options *broker.Options) {
+	//	options.Addrs = []string{"127.0.0.1:5672"}
+	//})
+	//
+	//// nats transportDrive
+	//transportDrive := nats.NewTransport(func(options *transport.Options) {
+	//	options.Addrs = []string{"127.0.0.1:4222"}
+	//})
 
 	service := micro.NewService(
 		micro.Name(microName),
-		micro.Registry(registerDrive),
-		micro.Broker(brokerDrive),
-		micro.Transport(transportDrive),
+		//micro.Registry(registerDrive),
+		//micro.Broker(brokerDrive),
+		//micro.Transport(transportDrive),
 	)
 
 	service.Init()
